75b34d0b30350f3b6cbf8ab6b31899cbab4659bb
Steve <15229911+stevehanstudio@users.noreply.github.com>
2022-07-04 02:11:42 -0700

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,11 +19,32 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:timestamp", (req, res) => {
+  console.log(req.params.timestamp)
+  const { timestamp } = req.params
+  if (/^[0-9]+$/.test(timestamp)) {
+    console.log('In seconds')
+    res.json({
+			unix: timestamp,
+			utc: new Date(+timestamp).toGMTString()
+		})
+  }
+  else if (/^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$/.test(timestamp)) {
+    console.log('Date format')
+    res.json({
+			unix: new Date(timestamp).getTime(),
+			utc: new Date(timestamp).toGMTString()
+		})
+  }
+  else {
+    res.json({ error: 'Invalid Date' })
+  }
+})
 
 
 // listen for requests :)
